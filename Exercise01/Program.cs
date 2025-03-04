b17df4d6233df3a34dffb49a62920c9ef95489ba
Souza284 <kanaan.pedro2@gmail.com>
2025-03-03 23:14:27 -0300

    Exercise01- Temperature converter

---
+++ Exercise01/Program.cs
@@ -0,0 +1,21 @@
+﻿// Exercise01- Temperature converter
+
+GetTemperature();
+
+static void GetTemperature()
+{
+    Console.WriteLine("Please enter in the temperature in celcius: ");
+    var temperature = decimal.Parse(Console.ReadLine()!);
+
+    Console.WriteLine(temperature + "C -> " + FahrenheitConverter(temperature) + "F -> " + KelvinConverter(temperature) + "K");
+}
+
+static decimal FahrenheitConverter(decimal _temperature)
+{
+    return _temperature * 1.8m + 32;
+}
+
+static decimal KelvinConverter(decimal _temperature)
+{
+    return _temperature + 273.5m;
+}
